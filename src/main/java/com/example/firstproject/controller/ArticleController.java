ebf45bc687ec7d1978c842b86ca4c231fd29306f
jhmin <hmin5015@gmail.com>
2022-12-04 10:21:42 -0800

    Create article repository with JPA to save data into database

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired // 스프링 부트가 미리 생성해 놓은 객체를 가져다가 자동 연결함.
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm(Model model) {
@@ -18,6 +24,13 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "";
     }
 }
